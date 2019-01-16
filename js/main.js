905e9689525dbb7d0f9ba932df8a138df30a45e8
Jose Dongil <jose.dongil@gmail.com>
2019-01-16 19:59:39 +0100

    refactor: added accessibility

--- js/main.js
+++ js/main.js
@@ -160,10 +160,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = 'Restaurant ' + restaurant.name
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -177,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', 'Go to details for restaurant ' + restaurant.name)
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
