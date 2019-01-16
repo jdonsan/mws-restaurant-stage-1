bfdb065f40aa1d9d23668abcd0c9f44250a4c5a1
Jose Dongil <jose.dongil@gmail.com>
2019-01-16 21:12:39 +0100

    refactor: added service worker

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  SWHelper.install();
   initMap(); // added 
   fetchNeighborhoods();
   fetchCuisines();
